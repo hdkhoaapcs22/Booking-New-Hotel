ad65b0bcdf071bab93db93279b6f6c43d2037b78
hdkhoaapcs22 <122841890+hdkhoaapcs22@users.noreply.github.com>
2024-01-24 07:36:27 +0700

    basic files

---
+++ booking_new_hotel/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'utils/themes.dart';
+import 'providers/theme_provider.dart';
+import 'motel_app.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+      .then((_) => runApp(_setAllProviders()));
+}
+
+Widget _setAllProviders() {
+  return MultiProvider(
+    providers: [
+      ChangeNotifierProvider(
+        create: (_) => ThemeProvider(
+          state: AppTheme.getThemeData,
+        ),
+      ),
+    ],
+    child: MotelApp(),
+  );
+}
