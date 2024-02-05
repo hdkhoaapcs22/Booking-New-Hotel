0d548a2b0e73dbe2c2f7f07118cc8d4d71949a91
hdkhoaapcs22 <122841890+hdkhoaapcs22@users.noreply.github.com>
2024-02-05 22:35:57 +0700

    update nescessary files

---
+++ booking_new_hotel/lib/models/popular_filter_list.dart
@@ -0,0 +1,58 @@
+class PopularFilterListData {
+  String titleTxt;
+  bool isSelected;
+  PopularFilterListData({
+    this.titleTxt = '',
+    this.isSelected = false,
+  });
+
+  static List<PopularFilterListData> popularFList = [
+    PopularFilterListData(
+      titleTxt: 'free_breakfast',
+      isSelected: false,
+    ),
+    PopularFilterListData(
+      titleTxt: 'free_Parking',
+      isSelected: false,
+    ),
+    PopularFilterListData(
+      titleTxt: 'pool_text',
+      isSelected: true,
+    ),
+    PopularFilterListData(
+      titleTxt: 'pet_friendly',
+      isSelected: false,
+    ),
+    PopularFilterListData(
+      titleTxt: 'Free_wifi',
+      isSelected: false,
+    ),
+  ];
+
+  static List<PopularFilterListData> accomodationList = [
+    PopularFilterListData(
+      titleTxt: 'all_text',
+      isSelected: false,
+    ),
+    PopularFilterListData(
+      titleTxt: 'apartment',
+      isSelected: false,
+    ),
+    PopularFilterListData(
+      titleTxt: 'Home_text',
+      isSelected: false,
+    ),
+    PopularFilterListData(
+      titleTxt: 'villa_data',
+      isSelected: false,
+    ),
+    PopularFilterListData(
+      titleTxt: 'hotel_data',
+      isSelected: false,
+    ),
+    PopularFilterListData(
+      titleTxt: 'Resort_data',
+      isSelected: false,
+    ),
+  ];
+}
---
+++ booking_new_hotel/lib/models/room_data.dart
@@ -0,0 +1,17 @@
+class RoomData {
+  int numberRoom;
+  int people;
+  RoomData(this.numberRoom, this.people);
+}
+
+class DateText {
+  late int stateDate;
+  late int endDate;
+
+  DateText(this.stateDate, this.endDate);
+}
+
+class PeopleSleeps {
+  int peopleNumber;
+  PeopleSleeps(this.peopleNumber);
+}
